ef6cf52230ea59bc3558e5926cfaf2d4ced38af0
Lucas Montalvao Bongalhardo <lucas.bongalhardo@sondait.com.br>
2020-10-07 12:21:12 -0300

    Adicionando lógica para vencerdor do jogo

--- src/index.js
+++ src/index.js
@@ -1,65 +1,108 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
 import './tic-tac-toe.css';
-class Square extends React.Component {
-  render() {
-    return (
-      React.createElement("button", { className: "square" }));
 
-
-
-  }}
-
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
+}
 
 class Board extends React.Component {
-  renderSquare(i) {
-    return React.createElement(Square, null);
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xisNext: true
+    };
   }
-
+  handleClick(i) {
+    const squares = this.squares.slice();
+    if(calculateWinner(squares)|| squares[i]){
+      return;
+    }
+    squares[i] = this.state.xisNext ? "X" : "O";
+    this.setState({ squares: squares });
+    this.setState({
+      squares: squares,
+      xisNext: !this.state.xisNext
+    })
+  }
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  };
   render() {
     const status = 'Next player: X';
 
     return (
       React.createElement("div", null,
-      React.createElement("div", { className: "status" }, status),
-      React.createElement("div", { className: "board-row" },
-      this.renderSquare(0),
-      this.renderSquare(1),
-      this.renderSquare(2)),
+        React.createElement("div", { className: "status" }, status),
+        React.createElement("div", { className: "board-row" },
+          this.renderSquare(0),
+          this.renderSquare(1),
+          this.renderSquare(2)),
 
-      React.createElement("div", { className: "board-row" },
-      this.renderSquare(3),
-      this.renderSquare(4),
-      this.renderSquare(5)),
+        React.createElement("div", { className: "board-row" },
+          this.renderSquare(3),
+          this.renderSquare(4),
+          this.renderSquare(5)),
 
-      React.createElement("div", { className: "board-row" },
-      this.renderSquare(6),
-      this.renderSquare(7),
-      this.renderSquare(8))));
-
-
-
-  }}
+        React.createElement("div", { className: "board-row" },
+          this.renderSquare(6),
+          this.renderSquare(7),
+          this.renderSquare(8))));
+  }
+}
 
 
 class Game extends React.Component {
   render() {
     return (
       React.createElement("div", { className: "game" },
-      React.createElement("div", { className: "game-board" },
-      React.createElement(Board, null)),
+        React.createElement("div", { className: "game-board" },
+          React.createElement(Board, null)),
 
-      React.createElement("div", { className: "game-info" },
-      React.createElement("div", null),
-      React.createElement("ol", null))));
+        React.createElement("div", { className: "game-info" },
+          React.createElement("div", null),
+          React.createElement("ol", null))));
 
 
 
-  }}
+  }
+}
 
 
 // ========================================
 
 ReactDOM.render(
-React.createElement(Game, null),
-document.getElementById('root'));
+  React.createElement(Game, null),
+  document.getElementById('root'));
+
+function calculateWinner(squares){
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ];
+
+  for(let i = 0; i < lines.length; i++){
+    const [a,b,c] = lines[i];
+
+    if(squares[a]&&squares[a]===squares[b]&&squares[a]===squares[c]){
+      return squares[a];
+    }
+  }
+  return null;
+}
