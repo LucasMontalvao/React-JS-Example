126abbdce38b3cf96b7fb159969861a01de8e3a7
Lucas Montalvao Bongalhardo <lucas.bongalhardo@sondait.com.br>
2020-10-07 10:01:30 -0300

    Tic-tac-toe first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          Hello World!
-        </h1>
-      </header>
-    </div>
-  );
+class ShoppingList extends React.Component {
+  render() {
+    return (
+      React.createElement("div", {
+        className: "shopping-list"
+      }, 
+      /*#__PURE__*/React.createElement("h1", null, "Shopping List for ", this.props.name), 
+      /*#__PURE__*/React.createElement("ul", null, 
+      /*#__PURE__*/React.createElement("li", null, "Instagram"), 
+      /*#__PURE__*/React.createElement("li", null, "WhatsApp"), 
+      /*#__PURE__*/React.createElement("li", null, "Oculus"))
+      ));
+  }
 }
-
-export default App;
+export default ShoppingList;
